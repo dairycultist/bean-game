42d77c019f3afe05ac025836979d7dc97856717d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-02-18 20:45:30 -0500

    Proper bean dropping position

--- bean_dropper.gd
+++ bean_dropper.gd
@@ -3,10 +3,32 @@ extends Node2D
 @export var bean: PackedScene
 @export var camera: Camera2D
 
+var the_bean: Bean
+
+func _ready() -> void:
+	
+	the_bean = bean.instantiate()
+	add_child(the_bean)
+	the_bean.set_color()
+	the_bean.freeze = true
+	the_bean.global_position.y = -166
+	the_bean.collision_layer = 0
+
+func _process(_delta: float) -> void:
+	the_bean.global_position.x = clamp((camera.get_local_mouse_position() + camera.position).x, -100, 100)
+
 func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int):
 
 	if event.is_action_pressed("click"):
-		var the_bean := bean.instantiate()
+		
+		# drop the_bean
+		the_bean.collision_layer = 1
+		the_bean.freeze = false
+		
+		# make a new the_bean
+		the_bean = bean.instantiate()
 		add_child(the_bean)
 		the_bean.set_color()
-		the_bean.global_position = camera.get_local_mouse_position() + camera.position
+		the_bean.freeze = true
+		the_bean.global_position.y = -166
+		the_bean.collision_layer = 0
